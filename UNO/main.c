b6ad715067f307ff8501ffd2062a5679fae462c7
XenZone <41632382+XenZone@users.noreply.github.com>
2019-09-26 12:58:07 -0400

    Add files via upload

--- UNO/main.c
+++ UNO/main.c
@@ -6,6 +6,13 @@
 #include <dirent.h>
 #include <time.h>
 
+
+typedef struct mazo{
+    char cartas[108][50];
+    int n_cartas;
+} mazo;
+
+
 DIR* directorio;
 
 void setup(){
@@ -41,6 +48,7 @@ void setup(){
                 strcpy(buffer, "");
             }
         }
+        strcpy(buffer, "");
         sprintf(buffer, "Juego/Mazo/0 %s.txt", colorCarta[color]);
         carta = fopen(buffer, "w");
         fclose(carta);
@@ -60,58 +68,25 @@ void setup(){
     }
 
 
-void repartir(){
-
-    //se reparten 7 cartas a cada jugador y se mueve una carta a Last
-
-    time_t t;
-    srand((unsigned) time(&t));
-
-    struct dirent* dEntrada;
-    directorio = opendir("./Juego/Mazo");
-
-    char** cardL;
-    cardL = (char**)malloc(108 * sizeof(char*));
-
-    int it = 0;
-    //while ((dEntrada = readdir(directorio))!= NULL){
-    while (it < 108){
-        cardL[it] = (char*)malloc(50 * sizeof(char));
-        cardL[it] = dEntrada->d_name;
-        it++;
-
-    }
-    for(it = 0; it < 108; it++){
-        printf("%s\n", cardL[it]);
-    }
-
-
-    int carta, jugador;
-    for(jugador = 1; jugador < 5; jugador++){
-        for(carta = 0; carta < 7; carta++){
-
-            int r = rand()%108;
-
-            char comm[100] = "mv ./Juego/Mazo/";
-            char strJ[1];
-            *strJ = jugador + '0';
-
-            strcat(comm,cardL[r]);
-            strcat(comm,"./Juego/Jugador_");
-            strcat(comm,strJ);
-
-            system(comm);
+mazo* leerMazo(){
+    mazo *Mazo = malloc(sizeof(mazo));
+    DIR *directorio;
+    struct dirent *dir;
+    int count = 0;
+    int index = 0;
+    directorio = opendir("Juego/Mazo/.");
+    if (directorio){
+        while ((dir = readdir(directorio)) != NULL){
+            if ((strcmp(dir->d_name, ".") != 0 && (strcmp(dir->d_name, "..") != 0))){
+                strcpy(Mazo->cartas[count],dir->d_name);
+                count++;
+            }
         }
+        Mazo->n_cartas = count;
+        closedir(directorio);
     }
-
-    int i;
-    for(i = 0; i < 108; i++){
-        free(cardL[i]);
-    }
-    free(cardL);
+    return Mazo;
 }
-
-
 int buscarPrevio(int situacion){
  
     char opcion;
@@ -119,42 +94,48 @@ int buscarPrevio(int situacion){
 
     if (situacion == 1){
         while (verificacion == 1){
-            printf("Existe una partida anterior guardada. Desea continuarla? (S|N)");
+            printf("Existe una partida anterior guardada. Desea continuarla? (S|N): ");
             scanf(" %c", &opcion);
 
             if (opcion == 'S'){
-                printf("Se retomaran los datos anteriores.");
+                printf("Se retomaran los datos anteriores.\n");
                 return 1;
             }
             else if (opcion == 'N'){
-                printf("La partida ha sido eliminada.");
+                printf("La partida ha sido eliminada.\n");
                 system("rm -r Juego");
                 return 0;
             }
             else{
-                printf("Entrada invalida");
+                printf("Entrada invalida\n");
             }
         }
     }
     
     else{
         while (verificacion == 1){
-            if(opendir("Juego")){    // Asi se revisa el directorio completo del juego, sin necesidad de revisar cada una de las carpetas
+            directorio = opendir("Juego");
+            if(directorio){    // Asi se revisa el directorio completo del juego, sin necesidad de revisar cada una de las carpetas
                 printf("La ultima partida no termino debidamente, y los datos de la partida siguen ahi. Desea eliminarlos? (S|N):\n");
                 scanf(" %c", &opcion);
                 if (opcion == 'S'){
                     system("rm -r Juego");  // Se elimina la carpeta padre, y se muere todo lo que est? adentro. izi pizi.
                     verificacion = 0;
-                    return 1;
+                    closedir(directorio);
+                    return 0;
                 }
                 else if (opcion == 'N'){
                     verificacion = 0;
-                    printf("Se retomar? la partida guardada anteriormente");
+                    printf("Se retomara la partida guardada anteriormente\n");
+                    closedir(directorio);
                     return 0;
                 }
-                else printf("Entrada inv?lida.");
+                else printf("Entrada invalida.\n");
             }
-            else return 0;
+            else{
+                return 0;
+                closedir(directorio);
+            }
         }
     }
     return 0; // Nunca deber?a llegar a este punto.
@@ -175,6 +156,7 @@ void terminarPartida(){
             Dummy = fopen("Juego/gameIsSaved/dummy.txt", "w");
             fprintf(Dummy, "Hola, si me ves aqui es porque grabaste tu partida, y me aseguro que la carpeta exista.\nLogre mi mision! Yupy!");
             verificacion = 0;
+            fclose(Dummy);
         }
         else if (opcion == 'N'){
             verificacion = 0;
@@ -182,27 +164,26 @@ void terminarPartida(){
             system("rm -r Juego");
             return;
         }
-        else printf("Entrada inv?lida.\n");
+        else printf("Entrada invalida.\n");
     }
     return; // Nunca deber?a llegar a este punto.
 }
 
 int main(){
-    
     int restaurar = 0;
-    if (opendir("Juego/gameIsSaved")){ 
+    directorio = opendir("Juego/gameIsSaved");
+    if (directorio){ 
         restaurar = buscarPrevio(1);
     }
     else restaurar = buscarPrevio(2);
+    closedir(directorio);
     
     if (restaurar != 1){
         setup();
-        puts("3,..............................................\n");
-        repartir();
     }
+    mazo *Mazo = leerMazo();
     
     terminarPartida();
-    
-    
+    free(Mazo);
     return 0;
 }
